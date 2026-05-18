398def073bb74d44067194a7b40ef7eca4a79722
Dylan Kawashiri <87044258+dylankawashiri@users.noreply.github.com>
2026-05-17 22:23:15 -0700

    param checker

---
+++ src/CuPyNLO/util/checker.py
@@ -0,0 +1,12 @@
+from typing import Any
+
+def checker(func: Any) -> Any:
+    def wrapper(self: Any, *args: Any, **kwargs: Any):
+        print(args, kwargs)
+        for kwarg in kwargs:
+            if kwargs[kwarg] == getattr(self, kwarg):
+                return None
+            else:
+                setattr(self, kwarg, kwargs[kwarg])
+        return func(self, *args, **kwargs)
+    return wrapper
