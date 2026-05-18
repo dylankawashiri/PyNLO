ce7028fe64b1b07d6d573f9ae2f57bf678d9e9f5
Dylan Kawashiri <87044258+dylankawashiri@users.noreply.github.com>
2026-05-17 23:04:15 -0700

    removed print function

--- src/CuPyNLO/util/checker.py
+++ src/CuPyNLO/util/checker.py
@@ -2,7 +2,6 @@ from typing import Any
 
 def checker(func: Any) -> Any:
     def wrapper(self: Any, *args: Any, **kwargs: Any):
-        print(args, kwargs)
         for kwarg in kwargs:
             if kwargs[kwarg] == getattr(self, kwarg):
                 return None
